e9400e419e7ab4f712e4c9591e8b787fc2e0df92
Junyoung Song <junyoungsong@Junyoungui-MacBookAir.local>
2022-07-19 02:12:54 +0900

    Moved 'fortune cookie' into module.

--- meadowlark.js
+++ meadowlark.js
@@ -1,3 +1,4 @@
+const fortune = require("./lib/fortune");
 const express = require("express");
 const expressHandlebars = require("express-handlebars");
 const app = express();
@@ -17,17 +18,8 @@ const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => res.render("home"));
 
-const fortunes = [
-  "Conquer your fears or they will conquer you.",
-  "Rivers need springs.",
-  "Do not fear what you don't know.",
-  "You will have a pleasant surprise.",
-  "Whenever possible, keep it simple.",
-];
-
 app.get("/about", (req, res) => {
-  const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-  res.render("about", { fortune: randomFortune });
+  res.render("about", { fortune: fortune.getFortune() });
 });
 
 //custom 404 page
